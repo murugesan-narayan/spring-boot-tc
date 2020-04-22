eb4c9677f3dec1340d3b75625c9d32b2e61ffc02
Murugesan <murugesan.narayan@gmail.com>
2020-04-22 22:30:11 +0200

    Initial commit

---
+++ src/test/java/pl/codeleak/samples/springboot/tc/OwnerRepositoryTests.java
@@ -0,0 +1,27 @@
+package pl.codeleak.samples.springboot.tc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// JPA tests with datasource configured via standard properties files
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class OwnerRepositoryTests {
+
+    @Autowired
+    private OwnerRepository ownerRepository;
+
+    @Test
+    void findAllReturnsJohnDoe() { // as defined in tc-initscript.sql
+        List<Owner> owners = ownerRepository.findAll();
+        assertThat(owners.size()).isOne();
+        assertThat(owners.get(0).getFirstName()).isEqualTo("John");
+        assertThat(owners.get(0).getLastName()).isEqualTo("Doe");
+    }
+}
